56136f911d787c5a34ffdda76515f7360b93538e
Martin Bornhold <mb@bo-tech.de>
2017-11-06 12:49:16 +0100

    Fix antiquotation in initial user json file

--- nixos/modules/services/taiga/initial_user_json.nix
+++ nixos/modules/services/taiga/initial_user_json.nix
@@ -4,8 +4,8 @@
     {
         "model": "users.user",
         "fields": {
-            "username": "cfg.adminUsername",
-            "full_name": "cfg.adminFullName",
+            "username": "${cfg.adminUsername}",
+            "full_name": "${cfg.adminFullName}",
             "bio": "",
             "lang": "",
             "color": "",
@@ -17,7 +17,7 @@
             "token": "",
             "last_login": "2013-04-04T07:36:09.880Z",
             "password": "",
-            "email": "cfg.adminEmail",
+            "email": "${cfg.adminEmail}",
             "date_joined": "2013-04-01T13:48:21.711Z"
         }
     }
