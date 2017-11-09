3cafa3c33d32cfe3b447bc2ffcb777c076a066a4
Martin Bornhold <mb@bo-tech.de>
2017-11-09 13:28:45 +0100

    Move db and db-user creation to it's own nixos module.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1,6 +1,7 @@
 [
-  ./services/taiga/taiga-back.nix
-  ./services/taiga/taiga-front.nix
   ./services/rhodecode/enterprise.nix
   ./services/rhodecode/vcsserver.nix
+  ./services/taiga/taiga-back.nix
+  ./services/taiga/taiga-front.nix
+  ./services/tools/create-dbs-and-users.nix
 ]
---
+++ nixos/modules/services/tools/create-dbs-and-users.nix
@@ -0,0 +1,113 @@
+# This is a simple helper service which creates missing databases and database
+# users for the postgresql service.
+
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.services.createDbsAndUsers;
+
+  # List of systemd units representing the deployment keys.
+  deploymentKeyUnits = concatMap (u: [ "${u.pwKey}-key.service" ]) cfg.users;
+
+  # Create an entry suitable for the pg_hba.conf file from an authentication
+  # attribute set from the "auths" option.
+  mkPgAuth = (auth: ''
+    ${auth.type} ${auth.database} ${auth.user} ${auth.address} ${auth.method}
+  '');
+
+  # Script that creates missing databases and users.
+  createMissingUsersAndDbs = pkgs.writeScript "createMissingUsersAndDbs.sh" ''
+    #! ${pkgs.bash}/bin/bash -e
+    export PATH=${pkgs.sudo}/bin:${config.services.postgresql.package}/bin:$PATH
+
+    # Create missing users.
+    users=( ${concatMapStringsSep " " (u: u.name) cfg.users} )
+    pw_paths=( ${concatMapStringsSep " " (u: (builtins.getAttr u.pwKey config.deployment.keys).path) cfg.users} )
+    for index in ''${!users[*]}; do
+      user=''${users[$index]}
+      pw_path=''${pw_paths[$index]}
+
+      user_exists=$(sudo -u postgres -- \
+        psql -q -t -A -d postgres -c \
+        "SELECT 1 AS exists FROM pg_user WHERE usename='$user';")
+      if [ -z $user_exists ]; then
+        password=$(cat $pw_path)
+        sudo -u postgres -- \
+          psql -c "CREATE USER $user WITH PASSWORD '$password';"
+      fi
+    done
+
+    # Create missing databases.
+    databases=( ${concatMapStringsSep " " (f: f.name) cfg.databases} )
+    owners=( ${concatMapStringsSep " " (f: f.owner) cfg.databases} )
+    for index in ''${!databases[*]}; do
+      database=''${databases[$index]}
+      owner=''${owners[$index]}
+
+      db_exists=$(sudo -u postgres -- \
+        psql -q -t -A -d postgres -c \
+        "SELECT 1 AS exists FROM pg_database WHERE datname='$database';")
+      if [ -z $db_exists ]; then
+        sudo -u postgres -- \
+          psql -c "CREATE DATABASE $database WITH OWNER '$owner';"
+      fi
+    done
+  '';
+
+in {
+
+  options.services.createDbsAndUsers = {
+
+    enable = mkEnableOption "Create databases and database users.";
+
+    users = mkOption {
+      type = types.listOf types.attrs;
+      default = [ ];
+      example = [ { name="myuser"; pwKey="myUserPassword"; } ];
+      description = ''
+        List of database users that shall exist.
+      '';
+    };
+
+    databases = mkOption {
+      type = types.listOf types.attrs;
+      default = [ ];
+      example = [ { name="mydb"; owner="myuser"; } ];
+      description = ''
+        List of databases that shall exist.
+      '';
+    };
+
+    authentications = mkOption {
+      type = types.listOf types.attrs;
+      default = [ ];
+      example = [ { user="myuser"; database="mydb"; type="host"; address="127.0.0.1/24"; method="md5"; } ];
+      description = ''
+        List of authentications.
+      '';
+    };
+
+  };
+
+  config = mkIf cfg.enable {
+
+    services.postgresql.authentication = (
+      lib.concatMapStringsSep "\n" mkPgAuth cfg.authentications);
+
+    systemd.services.createMissingUsersAndDbs = {
+      description = "Create missing databases and database users";
+      wantedBy = [ "multi-user.target" ];
+      wants = deploymentKeyUnits;
+      after = [ "network.target" "postgresql.service" ] ++ deploymentKeyUnits;
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${createMissingUsersAndDbs}";
+      };
+    };
+
+  };
+
+}
