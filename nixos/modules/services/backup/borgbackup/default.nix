6fbeef4363641bdcea426d9396baed18b8b48cf7
Martin Bornhold <mb@bo-tech.de>
2017-11-15 20:09:25 +0100

    Add empty scaffold for borg backup service.

---
+++ nixos/modules/services/backup/borgbackup/default.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+    cfg = config.services.borgbackup;
+in
+
+{
+
+  options = {
+    # TODO: declarations
+  };
+
+  config = {
+    # TODO: definitions
+  };
+
+}
