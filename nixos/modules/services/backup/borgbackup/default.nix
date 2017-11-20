9fdd354a9c53aea5a0d73b02175601837b347b57
Martin Bornhold <mb@bo-tech.de>
2017-11-20 22:24:32 +0100

    First bits of bog backup nixos module.

--- nixos/modules/services/backup/borgbackup/default.nix
+++ nixos/modules/services/backup/borgbackup/default.nix
@@ -3,17 +3,91 @@
 with lib;
 
 let
-    cfg = config.services.borgbackup;
-in
+  cfg = config.services.borgbackup;
 
-{
+createJobOptions = {
+  options = with types; {
 
-  options = {
-    # TODO: declarations
+    paths = mkOption {
+      type = listOf str;
+      default = [ ];
+      example = [ "/home/jane/Documents" "/home/jane/db.dump" ];
+      description = ''
+        The list of filesystem paths to backup during this create job. Shell
+        expansion can be used in these paths, see the borg manual for more
+        details.
+      '';
+    };
+
+    repo = mkOption {
+      type = str;
+      default = null;
+      example = "/path/to/borg/repository";
+      description = ''
+        Path to the borg backup repository to use as target for this create job.
+        See borg manual for possible choices like local file system path or
+        remote paths via ssh.
+      '';
+    };
+
+    archive = mkOption {
+      type = str;
+      default = null;
+      example = "{hostname}-{user}-{now:%Y-%m-%dT%H:%M:%S.%f}";
+      description = ''
+        Name of the archive to create. See borg manual for further options.
+      '';
+    };
+
+    systemdTimer = mkOption {
+      type = str;
+      default = null;
+      example = "borg-backup.timer";
+      description = ''
+        Name of the systemd timer unit to use for triggering execution of this
+        job.
+      '';
+    };
+
+  };
+};
+
+in {
+
+  options.services.borgbackup = with types; {
+
+    enable = mkEnableOption "Borg backup";
+
+    createJobs = mkOption {
+      type = listOf (submodule createJobOptions);
+      default = [ ];
+      description = ''
+        List of borg create jobs.
+      '';
+    };
+
   };
 
-  config = {
-    # TODO: definitions
+  config = mkIf (cfg.enable && cfg.createJobs != [ ]) {
+
+    systemd.services.borg-backup = {
+      description = "Exceute a borg backup create job";
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        Group = "root";
+      };
+      script = ''
+        borg create ${job.repo}::${job.archive} ${lib.concatStringsSep " " job.paths}
+      '';
+    };
+
+    systemd.timers.borg-timer = {
+      description = "Trigger borg backup unit";
+      wantedBy = [ "timers.target" ];
+      partOf = [ "borg-backup.service" ];
+    };
+
   };
 
 }
